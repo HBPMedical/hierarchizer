4e5031c379552bc78d25d87bcc612bc46bd33c63
Mirco Nasuti <mirco.nasuti@chuv.ch>
2017-02-17 16:34:59 +0100

    hierarchy and unknown value are arguments

--- hierarchize.py
+++ hierarchize.py
@@ -10,26 +10,26 @@ from glob import iglob
 from dicom.errors import InvalidDicomError
 
 
-HIERARCHY = ['PatientID', 'StudyID', 'SeriesDescription', 'SeriesNumber']
-UNKNOWN_VALUE = "unknown"
-
-
 def main():
     logging.basicConfig(level=logging.INFO)
 
     args_parser = argparse.ArgumentParser()
     args_parser.add_argument("input_folder")
     args_parser.add_argument("output_folder")
+    args_parser.add_argument("--attributes",
+                             nargs='+',
+                             default=['PatientID', 'StudyID', 'SeriesDescription', 'SeriesNumber'])
+    args_parser.add_argument("--unknown_value", default="unknown")
     args = args_parser.parse_args()
 
     for file_path in iglob(path.join(args.input_folder, "**/*"), recursive=True):
         try:
             dcm = dicom.read_file(file_path)
             dest_path = args.output_folder
-            for attribute in HIERARCHY:
+            for attribute in args.attributes:
                 part = str(dcm.data_element(attribute).value)
                 if len(part) < 1:
-                    part = UNKNOWN_VALUE
+                    part = args.unknown_value
                 dest_path += "/" + part
             dest_path = path.normpath(dest_path)
             makedirs(dest_path, exist_ok=True)

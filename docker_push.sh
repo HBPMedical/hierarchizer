fef774121980951e699694b1d000a733e31de423
Mirco Nasuti <mirco.nasuti@chuv.ch>
2017-03-06 15:02:00 +0100

    fix docker_push

--- docker_push.sh
+++ docker_push.sh
@@ -26,7 +26,7 @@ else
   CAPTAIN="sudo captain"
 fi
 
-$CAPTAIN push --branch-tags=false --commit-tags=true hierarchizer
+$CAPTAIN push --branch-tags=false --commit-tags=true hierarchizer --tag $(git describe)
 sed "s/USER/${USER^}/" $WORKSPACE/slack.json > $WORKSPACE/.slack.json
 sed -i.bak "s/VERSION/$(git describe)/" $WORKSPACE/.slack.json
 curl -k -X POST --data-urlencode payload@$WORKSPACE/.slack.json https://hbps1.chuv.ch/slack/dev-activity

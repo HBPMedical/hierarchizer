560f5bb6a73f9aa6890fadfb15dd64f9969724bd
Mirco Nasuti <mirco.nasuti@chuv.ch>
2017-03-31 12:16:00 +0200

    bugfix

--- hierarchizer/dicom_organizer.py
+++ hierarchizer/dicom_organizer.py
@@ -12,6 +12,7 @@ from . import ppmi_xml_extension
 def organize_dicom(input_folder, output_folder, organisation, excluded_fields, use_ppmi_xml_extension, unknown_value):
     logging.info("Organizing DICOM files...")
     for file_path in iglob(path.join(input_folder, "**/*"), recursive=True):
+        output_fullpath = output_folder
         try:
             dcm = dicom.read_file(file_path)
             for attribute in organisation:
@@ -25,9 +26,9 @@ def organize_dicom(input_folder, output_folder, organisation, excluded_fields, u
                         part = ppmi_xml_extension.find(file_path, attribute)
                     if not part:
                         part = unknown_value
-                output_folder = path.join(output_folder, part)
-            makedirs(output_folder, exist_ok=True)
-            logging.info("Copying %s to %s..." % (file_path, output_folder))
-            copy2(file_path, output_folder)
+                output_fullpath = path.join(output_fullpath, part)
+            makedirs(output_fullpath, exist_ok=True)
+            logging.info("Copying %s to %s..." % (file_path, output_fullpath))
+            copy2(file_path, output_fullpath)
         except (IsADirectoryError, InvalidDicomError):
             pass
